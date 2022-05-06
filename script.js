08937387f7815cfcb298a7edc10779dfe73f20fc
marko-zc <marko.zarkovic.cerneli@gmail.com>
2022-05-06 15:25:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstRow, secondRow;
+
+firstRow = $("#first-row > .image-row");
+secondRow = $("#second-row > .image-row");
+
+function onClickSliderRight() {
+    slideRight(firstRow);
+    slideRight(secondRow);
+}
+
+function onClickSliderLeft() {
+    slideLeft(firstRow);
+    slideLeft(secondRow)
+}
+
+function slideRight(sliderRow) {
+    const firstElement = sliderRow.children().first();
+    firstElement.hide().appendTo(sliderRow).animate({ width: 'toggle' }, "slow");
+}
+
+function slideLeft(sliderRow) {
+    const lastElement = sliderRow.children().last();
+    lastElement.animate({ width: 'toggle' }, { duration: "slow", complete: () => { lastElement.prependTo(sliderRow) } });
+}
