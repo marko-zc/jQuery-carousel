05c952a028a17cbb5cbe23034f08a4f118061f90
marko-zc <marko.zarkovic.cerneli@gmail.com>
2022-05-08 12:45:19 +0200

    Throttle slider buttons.

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 let firstRow, secondRow;
+let throttlePause = false;
 
 firstRow = $("#first-row > .image-row");
 secondRow = $("#second-row > .image-row");
 
 function onClickSliderRight() {
+    if (throttlePause) return;
+    throttlePause = true;
     slideRight(firstRow);
     slideRight(secondRow);
 }
 
 function onClickSliderLeft() {
+    if (throttlePause) return;
+    throttlePause = true;
     slideLeft(firstRow);
     slideLeft(secondRow);
 }
 
 function slideRight(sliderRow) {
     const firstElement = sliderRow.children().first();
-    firstElement.hide().appendTo(sliderRow).animate({ width: 'toggle' }, "slow");
+    firstElement.hide().appendTo(sliderRow).animate(
+        { width: 'toggle' },
+        {
+            duration: "slow",
+            complete: () => {
+                throttlePause = false;
+            }
+        }
+    );
 }
 
 function slideLeft(sliderRow) {
     const lastElement = sliderRow.children().last();
-    lastElement.animate({ width: 'toggle' }, { duration: "slow", complete: () => { lastElement.show().prependTo(sliderRow) } });
+    lastElement.animate(
+        { width: 'toggle' }, 
+        {
+            duration: "slow",
+            complete: () => {
+                lastElement.prependTo(sliderRow).show();
+                throttlePause = false;
+            }
+        }
+    );
 }
